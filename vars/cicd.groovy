70980d19ca4f5b0e723f2bbd6f9c596fd9a7a90a
sushma-suma <108732793+sushma-suma@users.noreply.github.com>
2022-07-19 18:29:22 +0530

    Create cicd.groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,14 @@
+def newGit(repo)
+{
+  echo "${repo}
+}
+def newMaven()
+{
+   sh 'mvn package'
+}
+def newDeploy(ip,pathname)
+{
+   deploy adapters: [tomcat9(credentialsId: '56669dda-ddff-4884-ae06-526da02164e4', path: '', url: ${ip})], contextPath: ${pathname}, war: '**/*.war'
+}
+
+         
